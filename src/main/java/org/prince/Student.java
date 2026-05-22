2e81e141abd3d4cdc56fc1af5bb83112947d18c9
Prince kumar <118436358+princeep@users.noreply.github.com>
2026-05-22 22:43:55 +0530

    Add main class to save Student object using Hibernate session

--- src/main/java/org/prince/Student.java
+++ src/main/java/org/prince/Student.java
@@ -5,5 +5,47 @@ public class Student {
     private String name;
     private int roll;
     private float marks;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public float getMarks() {
+        return marks;
+    }
+
+    public void setMarks(float marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", roll=" + roll +
+                ", marks=" + marks +
+                '}';
+    }
 }
 
